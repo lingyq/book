f097cfcea9bbbeade5feaaa7f18f81a0c33cab4f
lingyq <lyq.roco@foxmail.com>
2018-12-20 23:55:58 +0800

    apue3:     03_01lseek.c     03_02creat.c     03_05readwrite.c     03_11fcntl.c

---
+++ apue3/03/03_02creat.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+#include <fcntl.h>
+
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+// #define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+int
+main(void)
+{
+	
+	int fd;
+	
+	if((fd = creat("file.hole", FILE_MODE)) < 0) 
+		err_sys("creat error");
+	
+	if(write(fd, buf1, 10) != 10)
+		err_sys("buf1 write error");
+	/* offset now = 10 */
+	
+	if(lseek(fd,16384,SEEK_SET) == -1)
+		err_sys("lseek error");
+	/* offset now = 16384 */
+	
+	if(write(fd, buf2, 10) != 10)
+		err_sys("bufs write error");
+	/* offset now = 16394 */
+	
+	exit(0);
+}
